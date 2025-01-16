3c95bc438a2124727757aad2e6f7bf372fe32041
user <alimmamadovxurshed04@gmail.com>
2025-01-17 00:31:33 +0500

    Form Added

---
+++ app/components/Form.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+
+const Form = ({
+  title1,
+  title2,
+  subtitle1,
+  subtitle2,
+}: {
+  title1: string;
+  title2: string;
+  subtitle1: string;
+  subtitle2: string;
+}) => {
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 text-cosumDarkBlue">
+      <div className=" p-8 sm:p-0 w-full max-w-md">
+        <h2 className="text-4xl text-center font-bold mb-2 text-cosumDarkBlue">
+          {title1}
+        </h2>
+        <h3 className="text-4xl text-center font-bold mb-4">
+          {title2}
+        </h3>
+        <p className="text-center text-cosumDarkBlue mb-6">
+       {subtitle1}
+        </p>
+        <form className="space-y-4">
+          <div>
+            <label
+              className="block text-cosumDarkBlue font-bold mb-1"
+              htmlFor="name"
+            >
+              Your Name:
+            </label>
+            <input
+              type="text"
+              id="name"
+              placeholder="What is your name?"
+              className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+          <div>
+            <label
+              className="block text-cosumDarkBlue font-bold mb-1"
+              htmlFor="number"
+            >
+              Your Number:
+            </label>
+            <input
+              type="text"
+              id="number"
+              placeholder="Your number..."
+              className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+          </div>
+          <div>
+            <label
+              className="block text-cosumDarkBlue font-bold mb-1"
+              htmlFor="comments"
+            >
+              {subtitle2}
+            </label>
+            <textarea
+              id="comments"
+              placeholder="Your comment..."
+              className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              rows={4}
+            ></textarea>
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="w-full p-2 bg-blue-500 text-white font-bold rounded-lg hover:bg-blue-700"
+            >
+              Leave Request
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Form;
